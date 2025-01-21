03d14bb0c2fccbc706d7c47c3425c940dc4a2b30
Srini <ssunda03@calpoly.edu>
2025-01-20 17:07:36 -0800

    render.com deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import collectionRouter from "./routes/collectionRouter.js"
 import dotenv from 'dotenv'
 import discogs from "disconnect"
 
+import path from 'path'
+
 dotenv.config()
 
 const app = express()
@@ -14,8 +16,14 @@ const Discogs = new Client(process.env.USER_NAME, { userToken: process.env.USER_
 app.use(express.json())
 app.use("/collection", collectionRouter)
 
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))    
+})
+
 app.listen(process.env.PORT, () => {
-    console.log(`server started at http://localhost:${process.env.PORT}`)
+    console.log(`server started at port ${process.env.PORT}`)
 })
 
 export default Discogs
